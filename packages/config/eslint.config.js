7551366902ab8983afdd0991a13a0686b80d9ce0
Ashutosh Shukla <ashutosh6471@gmail.com>
2026-05-26 23:40:11 +0530

    chore: add workspace asset packages/config/eslint.config.js

---
+++ packages/config/eslint.config.js
@@ -0,0 +1,30 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { prefer: "type-imports", fixStyle: "inline-type-imports" },
+    ],
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      { checksVoidReturn: { attributes: false } },
+    ],
+    "@typescript-eslint/no-floating-promises": "error",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+  },
+  ignorePatterns: [
+    "node_modules/",
+    "dist/",
+    ".next/",
+    "*.config.js",
+    "*.config.cjs",
+    "*.config.mjs",
+  ],
+};
