fd86ca027dc667183cddfcb3fecbf874bee54b69
Ashutosh Shukla <ashutosh6471@gmail.com>
2026-05-26 23:40:27 +0530

    api: configure endpoints and trpc handlers for index

---
+++ packages/api/src/index.ts
@@ -0,0 +1,3 @@
+export { appRouter, type AppRouter } from "./root.js";
+export { createTRPCContext, createTRPCRouter, publicProcedure, protectedProcedure, orgProcedure } from "./trpc.js";
+export type { TRPCContext } from "./trpc.js";
